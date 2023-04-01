668c11c08cecf10a50216278a50f75c40da97727
sun <skkimeo@gmail.com>
2023-04-01 15:53:38 +0900

    solution: 미로 탈출 명령어

--- programmers/미로-탈출-명령어-sun.swift
+++ programmers/미로-탈출-명령어-sun.swift
@@ -1,140 +1,50 @@
 /*
 - https://school.programmers.co.kr/learn/courses/30/lessons/150365
+- dfs
+- 나는 원래 빡구현에 가까운 풀이로 풀었는데 다익스트라로 최단 경로 구한 다음에 이걸 활용하는 dp로 접근해보려다가 실패해서
+  어쨌든 가장 짧은 경로가 최단 경로를 기반으로 하는 건 맞아서 여러가지 예외를 다 고려해서 최단 경로 계산하는 로직을 짰다... 
+  근데 도출 과정도 너무 오래 걸리고 하나만 놓쳐도 바로 실수하기 좋아서 다른 사람들 풀이를 봤는데 dfs + 가지치기인거에요... 
+- 가지를 어케치냐가 관건인데 일단 임의의 좌표에서 남은 이동 수(remain)와 해당 위치에서 탈출 위치까지의 최단 거리(shortestRoute)를 사용해야 한다. 
+  임의의 점이 있으면 그냥 abs(end.x - x) + abs(end.y - y)가 최단 거리가 된다는 걸 생각을 못했는데 이거 생각한 분 천잰듯 ㄹㅇ
+  암튼 불가능한 경로를 제거하기 위해 2가지로 가지치기를 해주는 데 임의의 한 좌표에서 
+  1) (remain % 2 != shortestRoute)면 "원천적으로 탈출 불가"한 경우이므로 바로 중단한다 
+    - 이거는 나도 깨달은? 조건이었는데 이렇게 간결하게 표현할 수 있다니... 
+  2) (remain < shortestRoute)인 경우 문제의 조건인 "k회 안에 탈출 불가"한 경우이므로 바로 중단한다 
+    - 이게 더 천재임 미쳤음.. 
+  암튼 교훈은 걍 여러 경우의 수 중에 특정 조건을 만족하는 경로를 찾으라고 하면 가지치기를 어케할 지 고민하는 게 나을 듯 ㅠ 
 */
 
 import Foundation
 
-typealias Coordinate = (r: Int, c: Int)
-typealias Step = (dr: Int, dc: Int, dir: String)
+typealias Status = (x: Int, y: Int, count: Int, route: String)
 
-enum Path: String, CaseIterable {
-    case down = "d"
-    case left = "l"
-    case right = "r"
-    case up = "u"
-
-    var diff: Coordinate {
-        switch self {
-        case .down:
-            return (1, 0)
-        case .left:
-            return (0, -1)
-        case .right:
-            return (0, 1)
-        case .up:
-            return (-1, 0)
-        }
-    }
-}
-
-struct Queue<T> {
-    var isEmpty: Bool { startIndex == endIndex }
-    var count: Int { endIndex - startIndex }
-
-    private var startIndex = 0
-    private var endIndex: Int { self.elements.endIndex }
-    private var elements = [T]()
-
-    init(_ elements: [T]) {
-        self.elements = elements
-    }
-
-    mutating func enqueue(_ element: T) {
-        elements.append(element)
-    }
-
-    mutating func dequeue() -> T {
-        defer { startIndex += 1 }
-
-        return elements[startIndex]
-    }
-}
-
-private let fail = "impossible"
-private let dirs = Path.allCases.map { (r: $0.diff.r, c: $0.diff.c, dir: $0.rawValue) }
-
-/// 크기(n, n), 시작(x, y), 도착(r, c), 목표 거리: k
 func solution(_ n:Int, _ m:Int, _ x:Int, _ y:Int, _ r:Int, _ c:Int, _ k:Int) -> String {
-    let graph = makePaddedGraph(n: n, m: m), start = (x, y), end = (r, c)
-    let shortestPath = findShortestPath(graph: graph, start: start, end: end)
-    guard isPossibleLength(k, shortestLength: shortestPath.count)
-    else {
-        return fail
-    }
+    var stack = [Status(x: x, y: y, count: 0, route: "")]
+    let dirs = [(1, 0, "d"), (0, -1, "l"), (0, 1, "r"), (-1, 0, "u")].reversed()
 
-    return findBestFit(graph: graph, shortestPath: shortestPath, start: start, k: k)
-}
-
-func findBestFit(graph: [[Bool]], shortestPath: [Step], start: Coordinate, k: Int) -> String {
-    var k = k, shortestPath = shortestPath, free = (k - shortestPath.count) / 2
-    shortestPath.reverse()
-    var (r, c) = start, route = "", remains = [Step]()
-
-    while k != 0 {
-        k -= 1
-
-        if free == 0 {
-            let shouldPopRemains = (remains.last?.dir ?? "z") < (shortestPath.last?.dir ?? "z")
-            let (dr, dc, path) = shouldPopRemains ? remains.removeLast() : shortestPath.removeLast()
-            r += dr
-            c += dc
-            route += path
-
-            continue
+    while let status = stack.popLast() {
+        if (status.x, status.y, status.count) == (r, c, k) {
+            return status.route
         }
 
-        // find best dir
-        let index = dirs.firstIndex { graph[r + $0.r][c + $0.c] }!
-        let (dr, dc, dir) = dirs[index]
-        r += dr
-        c += dc
-        route += dir
-
-        guard (dr, dc) != (shortestPath.last?.dr ?? -1, shortestPath.last?.dc ?? -1)
-        else {
-            _ = shortestPath.popLast()
-            continue
-        }
-
-        free -= 1
-        remains.append((dr * -1, dc * -1, dirs[4 - index - 1].dir))
-    }
-
-    return route
-}
-
-func isPossibleLength(_ k: Int, shortestLength: Int) -> Bool {
-    k < shortestLength ? false : (k - shortestLength) % 2 == 0
-}
-
-func findShortestPath(graph: [[Bool]], start: Coordinate, end: Coordinate) -> [(Int, Int, String)] {
-    var graph = graph, queue = Queue([(start.r, start.c, [Path]())])
-
-    while !queue.isEmpty {
-        let (r, c, route) = queue.dequeue()
-        guard graph[r][c]
+        let remain = k - status.count, shortestRoute = abs(status.x - r) + abs(status.y - c)
+        guard 1...n ~= status.x,
+              1...m ~= status.y,
+              remain >= shortestRoute,
+              remain % 2 == shortestRoute % 2
         else {
             continue
         }
 
-        guard (r, c) != end
-        else {
-            return route.map { ($0.diff.r, $0.diff.c, $0.rawValue) }
-        }
-
-        graph[r][c].toggle()
-        for path in Path.allCases {
-            let (dr, dc) = path.diff
-            queue.enqueue((r + dr, c + dc, route + [path]))
+        for (dx, dy, path) in dirs {
+            stack.append(Status(
+                x: status.x + dx,
+                y: status.y + dy,
+                count: status.count + 1,
+                route: status.route + path
+            ))
         }
     }
 
-    return []
-}
-
-func makePaddedGraph(n: Int, m: Int) -> [[Bool]] {
-    let topBottom = Array(repeating: false, count: m + 2)
-    let mid = [false] + Array(repeating: true, count: m) + [false]
-
-    return [topBottom] + (0..<n).map { _ in mid } + [topBottom]
+    return "impossible"
 }
