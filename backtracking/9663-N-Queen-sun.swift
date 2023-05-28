33cfab834125f765306ede47e0c70e31970ea456
sun <skkimeo@gmail.com>
2023-05-28 21:41:36 +0900

    sun solved: 9663 N-Queen 개선

--- backtracking/9663-N-Queen-sun.swift
+++ backtracking/9663-N-Queen-sun.swift
@@ -1,20 +1,24 @@
 /*
- - https://www.acmicpc.net/problem/9663
- - 백트래킹(dfs)
- - 모르겠어요...모르겠어요....
- - 여러 솔루션을 보고 1차원 배열 + 대각선은 shift 개념을 사용해야 된다는 건 알았는데 아래 블로그 풀이가 시간 복잡도가 훨씬 개선되는데
-   아무리 봐도 이해를 못하겠다..gg^^
-    - 미래의 나야...화이팅...https://hyun083.tistory.com/63
- */
+- https://www.acmicpc.net/problem/9663
+- 백트래킹(dfs)
+
+- 주어진 칸에 퀸을 놓을 수 있는지를 확인하기 위해서 기존 퀸의 위치를 배열에 담고 배열의 각 값과 비교할 수도 있지만 
+   이 경우 배열의 크기와 걸리는 시간이 비례해서 증가함
+- 따라서 이를 O(1)안에 확인할 수 있도록 개선할 수 있는데 열, 왼쪽 대각선, 오른쪽 대각선 이렇게 3가지 경우마다 
+  visited 배열을 선언해주고 셋 다 아직 방문되지 않은 경우에만 퀸을 놓도록 하면 된다!
+*/
 
 import Foundation
 
-func solution(N: Int) -> Int {
-    let above = Array(repeating: true, count: N)
-    let leftDiagonal = Array(repeating: true, count: N)
-    let rightDiagonal = Array(repeating: true, count: N)
+print(solution(Int(String(readLine()!))!))
 
-    func dfs(row: Int, above: [Bool], leftDiagonal: [Bool], rightDiagonal: [Bool]) -> Int {
+func solution(_ N: Int) -> Int {
+    var down = Array(repeating: false, count: N)
+    /// 합, 차는 최대값이 2 * N 이므로 배열을 더 크게 선언
+    var diagonalLeft = Array(repeating: false, count: 2 * N)
+    var diagonalRight = diagonalLeft
+
+    func nQueen(row: Int = 0) -> Int {
         guard row != N
         else {
             return 1
@@ -22,30 +26,25 @@ func solution(N: Int) -> Int {
 
         var answer = 0
         for col in 0..<N {
-            guard above[col], leftDiagonal[col], rightDiagonal[col]
+            let sum = row + col
+            /// 음수가 되는 걸 막아야 하는데 abs로 하면 서로 가운데를 제외하고 두 줄씩 값이 같아지므로 N을 더함
+            let diff = N + row - col
+            guard !down[col], !diagonalRight[diff], !diagonalLeft[sum]
             else {
                 continue
             }
 
-            var above = above, leftDiagonal = leftDiagonal, rightDiagonal = rightDiagonal
-            leftDiagonal.removeLast()
-            leftDiagonal.insert(true, at: 0)
-            rightDiagonal.removeFirst()
-            rightDiagonal.append(true)
-            above[col] = false
-            if col + 1 < N {
-                leftDiagonal[col + 1] = false
-            }
-            if col - 1 >= 0 {
-                rightDiagonal[col - 1] = false
-            }
-            answer += dfs(row: row + 1, above: above, leftDiagonal: leftDiagonal, rightDiagonal: rightDiagonal)
+            down[col].toggle()
+            diagonalRight[diff].toggle()
+            diagonalLeft[sum].toggle()
+            answer += nQueen(row: row + 1)
+            down[col].toggle()
+            diagonalRight[diff].toggle()
+            diagonalLeft[sum].toggle()
         }
 
         return answer
     }
 
-    return dfs(row: 0, above: above, leftDiagonal: leftDiagonal, rightDiagonal: rightDiagonal)
+    return nQueen()
 }
-
-print(solution(N: Int(readLine()!)!))
